94ea0ca9b9a2854a50ecd7ec79cd4fcd13683033
Sergio Mironescu <xexiu.sj@gmail.com>
2014-12-20 02:31:52 +0000

    Validation title length

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :categories, presence: true, :inclusion => { :in => ['The Void', 'HTML + CSS','Javascript','Ruby'] }
   validates :user_id, presence: true
-  validates :title, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 110 }
   validates :body, presence: true, length: { minimum: 10 }
 end
