0de4f444cf84e0bdd30fb6c0ea069104ab92d75f
Sergio Mironescu <xexiu.sj@gmail.com>
2014-12-22 15:24:00 +0000

    Added show comments view + fixed css style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
+  
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
 
   def create
-    if simple_captcha_valid?
+    if simple_captcha_valid? || current_user.admin?
       @post = Post.find(params[:post_id])
       @comment = @post.comments.create(comment_params)
       redirect_to post_path(@post)
