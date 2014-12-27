9b41bbf0ef8eeaffe1990d3cca78ebd279279287
Sergio Mironescu <xexiu.sj@gmail.com>
2014-12-27 01:39:28 +0000

    FIX

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true # belongs_to :post
   has_many :comments, :as => :commentable
   validates :name, presence: true, length: { minimum: 3, maximum: 55 }
-  validates :body, presence: true, length: { minimum: 3, maximum: 2000 }
+  validates :body, presence: true, length: { minimum: 6, maximum: 2000 }
   
   def post
     return @post if defined?(@post)
