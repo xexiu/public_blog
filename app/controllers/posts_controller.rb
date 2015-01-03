e712e529af6abf54c684977e6f6a97947c6006ad
Sergio Mironescu <xexiu.sj@gmail.com>
2015-01-03 00:33:36 +0000

    FIX

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
     if params[:search]
       @posts = Post.search(params[:search]).order("created_at DESC").paginate(page: params[:page],  :per_page => 20)
     else
-      @posts = Post.all.order("featured_post ASC, created_at DESC, random()")
+      @posts = Post.all.order("featured_post ASC, random()").order("created_at DESC")
     end
   end
 
