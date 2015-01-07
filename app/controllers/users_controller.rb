9a85e9a3694e4c83223b0eef12a821c9477f2c22
Sergio Mironescu <xexiu.sj@gmail.com>
2015-01-07 17:44:43 +0000

    Fix user can now edit hiself but cannot edit other if not admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, :admin_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:edit, :update, :destroy]
+  before_action :logged_in_user, :correct_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  #before_action :correct_user, :admin_user,   only: [:edit, :update]
+  #before_action :admin_user,     only: [:edit, :update, :destroy]
   attr_accessor :name, :email
 
   def index
