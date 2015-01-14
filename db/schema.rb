8599660936b02e2392bd267941d44aed9591c708
Sergio Mironescu <xexiu.sj@gmail.com>
2015-01-14 00:19:19 +0000

    FIX

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113180729) do
+ActiveRecord::Schema.define(version: 20150113234832) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
