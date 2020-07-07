5e3f1beac75f497abe476e4acc1d1c06e4c1fdb8
smironescu <sergio.mironescu@marfeel.com>
2020-07-07 09:53:58 -0500

    First intent to fix assets on PROD

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( custom.js )
 Rails.application.config.assets.precompile += %w( google_analytics.js )
+Rails.application.config.assets.precompile << ["*.svg", "*.eot", "*.woff", "*.ttf"]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
